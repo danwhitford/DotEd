8baeb7afa521d8d204359bf3b83d618c90a1742e
dw185143 <dw185143@ncr.com>
2018-12-20 17:02:24 +0000

    jslint changed

--- src/scripts/doted.js
+++ src/scripts/doted.js
@@ -1,11 +1,13 @@
-
+'use strict';
+/*jslint browser:true */
+/*global d3*/
 
 function dan_make_graph(DOTstring) {
 
-    const w = document.getElementById('mynetwork').getBoundingClientRect().width;
+    var w = document.getElementById('mynetwork').getBoundingClientRect().width;
 
     d3.select("#mynetwork")
-        .graphviz() 
+        .graphviz()
             .width(w)
             .fit(true)
             .zoom(false)
@@ -14,16 +16,16 @@ function dan_make_graph(DOTstring) {
 }
 
 function update(ev) {
-    const target = ev.currentTarget;
-    if (target != null)
+    var target = ev.currentTarget;
+    if (target !== null) {
         dan_make_graph(target.value);
-    
+    }
 }
 
-window.onload = function() {
-    const mytext = document.getElementById('mytext');
-    if (mytext != null) {
+window.onload = function () {
+    var mytext = document.getElementById('mytext');
+    if (mytext !== null) {
         mytext.oninput = update;
         dan_make_graph(mytext.value);
     }
-}
+};
