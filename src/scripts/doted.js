bf960b65ea1618c61fd7558feba92db1745316ac
dw185143 <dw185143@ncr.com>
2018-12-21 11:00:41 +0000

    borders

--- src/scripts/doted.js
+++ src/scripts/doted.js
@@ -5,7 +5,6 @@
 function dan_make_graph(DOTstring) {
 
     var w = document.getElementById('mynetwork').getBoundingClientRect().width;
-    var h = document.getElementById('mynetwork').getBoundingClientRect().height;
 
     d3.select("#mynetwork")
         .graphviz()
